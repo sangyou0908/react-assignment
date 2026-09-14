2e9379ad09672806e0a656b39c75edc8a9104c74
“sangyou0908” <sangyou0908@gmail.com>
2026-09-14 21:33:28 +0900

    feat: movie 데이터 이관 및 MovieList 노출

--- day27/movie-app/src/App.jsx
+++ day27/movie-app/src/App.jsx
@@ -1,23 +1,12 @@
 import "./App.css";
-import MovieCard from "./components/MovieCard";
+import MovieList from "./components/MovieList";
 
-// 영화 데이터 (제공)
-const movie1 = {
-  title: "인셉션",
-  rating: 8.8,
-  poster: "https://image.tmdb.org/t/p/w500/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg",
-};
-
-const movie2 = {
-  title: "인터스텔라",
-  rating: 8.6,
-  poster: "https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
-};
-
-const movie3 = {
-  title: "다크 나이트",
-  rating: 9.0,
-  poster: "https://image.tmdb.org/t/p/w500/qJ2tCh2ThA2To2T93qCHYm4O3R8.jpg",
-};
+function App() {
+  return (
+    <main>
+      <MovieList></MovieList>
+    </main>
+  );
+}
 
 export default App;
--- day27/movie-app/src/components/MovieList.jsx
+++ day27/movie-app/src/components/MovieList.jsx
@@ -1,5 +1,24 @@
 import MovieCard from "./MovieCard";
 
+// 영화 데이터 (제공)
+const movie1 = {
+  title: "인셉션",
+  rating: 8.8,
+  poster: "https://image.tmdb.org/t/p/w500/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg",
+};
+
+const movie2 = {
+  title: "인터스텔라",
+  rating: 8.6,
+  poster: "https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
+};
+
+const movie3 = {
+  title: "다크 나이트",
+  rating: 9.0,
+  poster: "https://image.tmdb.org/t/p/w500/qJ2tCh2ThA2To2T93qCHYm4O3R8.jpg",
+};
+
 function MovieList() {
   return (
     <main className="container">
